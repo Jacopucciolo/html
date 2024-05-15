4bd892936e6a14e6bb068ae92bf24ca6f9739d2c
Jacopucciolo <76064436+Jacopucciolo@users.noreply.github.com>
2024-05-15 17:09:32 +0200

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,8 +1,11 @@
 function f() {
+    document.getElementById("second_message").innerHTML="";
     var overlay = document.getElementById("overlay");
     overlay.classList.toggle("show");
 
     setTimeout(function() {
         overlay.classList.toggle("show");
+        var secMes= document.getElementById("second_message");
+        secMes.innerHTML="Fammelo un sorriso ora, dai<br>♥"
     }, 12000);
 }
