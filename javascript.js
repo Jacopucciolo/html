4174afb14df1fece47aecf04e714497f31023c8d
Jacopucciolo <76064436+Jacopucciolo@users.noreply.github.com>
2024-05-15 17:41:04 +0200

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -6,6 +6,19 @@ function f() {
     setTimeout(function() {
         overlay.classList.toggle("show");
         var secMes= document.getElementById("second_message");
-        secMes.innerHTML="Fammelo un sorriso ora, dai<br>♥"
+        var nome = sessionStorage.getItem("name");
+        secMes.innerHTML="Fammelo un sorriso ora "+nome+", dai<br>♥"
     }, 12000);
+}
+function f2()
+{
+ var nome = document.getElementById("nm2").value;
+ sessionStorage.setItem("name", nome);
+ document.getElementById("form").submit();
+}
+function f3()
+{
+    var nome=sessionStorage.getItem("name");
+    document.getElementById("premimi-button").textContent=""+nome+", ecco una sorpresina per te"
+    alert(nome);
 }
