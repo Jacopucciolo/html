c8a6496fe4bf66ee499fdd201a27908f358b73b0
Jacopucciolo <76064436+Jacopucciolo@users.noreply.github.com>
2024-05-15 16:36:50 +0200

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,19 +1,8 @@
 function f() {
     var overlay = document.getElementById("overlay");
-    overlay.classList.add("show");
+    overlay.classList.toggle("show");
 
     setTimeout(function() {
-        overlay.classList.remove("show");
-        var button = document.getElementById("premimi-button");
-        //button.style.backgroundColor = getRandomColor();
+        overlay.classList.toggle("show");
     }, 12000);
-}
-
-function getRandomColor() {
-    //var letters = '0123456789ABCDEF';
-    //var color = '#';
-    //for (var i = 0; i < 6; i++) {
-    //    color += letters[Math.floor(Math.random() * 16)];
-    //}
-    //return color;
 }
